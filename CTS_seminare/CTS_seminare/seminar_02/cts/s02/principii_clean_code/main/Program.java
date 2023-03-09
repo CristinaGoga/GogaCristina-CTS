15eb430b78fe1a6bcb4db94ea510444c95b5a22b
criss <criss@192.168.1.144>
2023-03-09 15:45:13 +0200

    Commit Seminar 3

--- CTS_seminare/CTS_seminare/seminar_02/cts/s02/principii_clean_code/main/Program.java
+++ CTS_seminare/CTS_seminare/seminar_02/cts/s02/principii_clean_code/main/Program.java
@@ -1,21 +1,27 @@
 package cts.s02.principii_clean_code.main;
-
+import cts.s02.principii_clean_code.clase.Angajat;
 import java.io.FileNotFoundException;
 import java.util.List;
 
 import cts.s02.principii_clean_code.clase.Aplicant;
 import cts.s02.principii_clean_code.clase.Utils;
 import cts.s02.principii_clean_code.clase.reader.ReaderAngajat;
+import cts.s02.principii_clean_code.clase.Aplicant;
 
 public class Program {
 
 	public static void main(String[] args) {
-		ReaderAngajat readerAngajat=new ReaderAngajat();
+		ReaderAngajat readerAngajat = new ReaderAngajat();
 		List<Aplicant> listaAngajati;
 		try {
 			listaAngajati = readerAngajat.readAplicants("angajati.txt");
-			for(Aplicant angajat:listaAngajati)
+			Angajat.setSumaFinantare(100);
+			for(Aplicant angajat:listaAngajati) {
 				System.out.println(angajat.toString());
+				angajat.afisareSumaFinantare();
+				angajat.afisareStatus();
+			}
+				
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
